255cc42f2942b7938bac7c2496c2b912414344c7
Raman65 <rp0842262@gmail.com>
2024-08-22 09:06:11 +0530

    JavaScript variables, Data Types & Objects

---
+++ video55/script.js
@@ -0,0 +1,39 @@
+console.log("Hey this is tutorial 55");
+
+let a = 5;
+let b = 6;
+let c = "Raman";
+
+// console.log(a + b);
+// console.log(c);
+// console.log(typeof a, typeof b, typeof c);
+{
+    let a = 66;
+    console.log(a);
+}
+console.log(a);
+
+let x = "Raman cs";
+let y = 22;
+let z = 3.44;
+const p = true;
+let q = undefined;
+let r = null;
+console.log(x, y, z, p, q, r);
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+let o = {
+    name : "Raman",
+    "Roll no" : 18,
+    "Is handsome" : true
+}
+
+console.log(o);
+o.salary = "100K"
+console.log(o);
+o.salary = "500K"
+console.log(o);
+
+
+
+
