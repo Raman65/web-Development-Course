ee74b9139ecb9a50c83ed44bcf34ec31944c4c5a
Raman65 <rp0842262@gmail.com>
2024-08-22 19:40:34 +0530

    Exercise 9

---
+++ video59/index.js
@@ -0,0 +1,16 @@
+/*Create a faulty calculator using JavaScript
+
+This faulty calculator does following:
+1. It take two number as input from the user
+2. It perform wrong operation as follows:
++ ---> -
+* ---> +
+- ---> /
+/ ---> **
+
+It perform wrong operation 10% of the times
+
+a = Math.random()
+*/
+console.log("Raman");
+
