fab9b0771d234ec75322a7f2841abc75745ef10d
Raman65 <rp0842262@gmail.com>
2024-08-22 07:44:44 +0530

    Java script intro

---
+++ video54/script.js
@@ -0,0 +1,17 @@
+alert("Hello World")
+console.log("Code is running..");
+
+var a = prompt("Enter your number")
+var isTrue = confirm("Are you sure you to leave this page and burn your computer")
+if (isTrue) {
+    console.log("Computer is burn");
+    
+}
+else{
+    console.log("Computer not is burn");
+}
+console.log("Your number is "+a);
+
+document.title="Hey i am good."
+
+document.body.style.backgroundColor = "blue"
