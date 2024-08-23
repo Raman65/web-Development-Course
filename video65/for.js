5d8450dddaf49a32248c2e610b2173ca663d8d72
Raman65 <rp0842262@gmail.com>
2024-08-23 08:18:49 +0530

    Exercise 11

---
+++ video65/for.js
@@ -0,0 +1,12 @@
+let arr = [1,2,3,4,5]
+
+const red =(a,b)=>{
+    for (let i = 1; i <= arr.length; i++) {
+        // const element = a[i];
+        return a*b
+    }
+    
+    
+}
+
+console.log(arr.reduce(red));
---
+++ video65/script.js
@@ -0,0 +1,7 @@
+let a = [1,2,3,4,5]
+
+const red =(a,b)=>{
+        return a*b
+}
+
+console.log(a.reduce(red));
