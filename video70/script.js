4756ab7f5ca64c89f1be62305b94cb01a92ae6ae
Raman65 <rp0842262@gmail.com>
2024-08-23 20:29:21 +0530

    JavaScript Exercise 12

---
+++ video70/script.js
@@ -0,0 +1,14 @@
+console.log("Raman Pradhan");
+
+document.getElementById("r1").style.backgroundColor = "blue" 
+document.getElementById("r2").style.backgroundColor = "black" 
+document.getElementById("r3").style.backgroundColor = "aqua" 
+document.getElementById("r4").style.backgroundColor = "purple" 
+document.getElementById("r5").style.backgroundColor = "blueviolet" 
+
+
+document.getElementById("r1").style.color = "aqua" 
+document.getElementById("r2").style.color = "white" 
+document.getElementById("r3").style.color = "pink" 
+document.getElementById("r4").style.color = "wheat" 
+document.getElementById("r5").style.color = "brown" 
