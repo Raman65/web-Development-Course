b26c5f3db03beb652d022394190f317e0bbff599
Raman65 <rp0842262@gmail.com>
2024-08-23 19:50:19 +0530

    JavaScript Exercise 11 Solution

---
+++ video69/loop.js
@@ -0,0 +1,10 @@
+
+function factorial(number){
+    let fac = 1;
+    for (let i = 1; i <= number; i++) {
+        const element = number;
+        fac = fac*i;
+    }
+    return fac;
+}
+factorial(fac)
