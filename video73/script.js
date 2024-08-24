1b395e16a4241fdfe82f0808bc7a16b6ec56ab95
Raman65 <rp0842262@gmail.com>
2024-08-24 07:02:04 +0530

    JavaScript Exercise 13

---
+++ video73/script.js
@@ -0,0 +1,5 @@
+function createCard(title, cName, views, monthOld, duration,thumbnail){
+
+}
+
+createCard("Introduction to Backend | Sigma Web Dev video #12", "RamanPradhan", 570000, 7, "31.22", "https://i.ytimg.com/vi/tVzUXW6siu0/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLB0alxLSXCSEPITzWr-XXUiv1oglQ")
