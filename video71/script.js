15ad3be3f19f346fb8f10e7250598982258fcec1
Raman65 <rp0842262@gmail.com>
2024-08-24 06:44:20 +0530

    Inserting and Removing Element using JavaScript

---
+++ video71/script.js
@@ -0,0 +1,8 @@
+// let div = document.createElement("div")
+// div.innerHTML = "I have been inserted <b>by Raman</b>"
+// div.setAttribute("class", "created")
+// document.querySelector(".container").prepend(div)
+
+
+let cont = document.querySelector(".container")
+cont.insertAdjacentHTML("beforebegin", "<b> I am under the water please help me<b>")
