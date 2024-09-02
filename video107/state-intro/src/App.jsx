0b267b7a6cd21ab7067b448dea5584355abbbe3c
Raman65 <rp0842262@gmail.com>
2024-09-02 20:33:34 +0530

    Hooks & State in React

---
+++ video107/state-intro/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(2)
+
+  return (
+    <>
+      <div>The count is {count}</div>
+      <button onClick={()=>{setCount(count*2)}}>Update count</button>
+    </>
+  )
+}
+
+export default App
