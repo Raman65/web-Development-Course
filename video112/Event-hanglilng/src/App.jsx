510868fc820851eab94bfa5e2866656f84fcaddd
Raman65 <rp0842262@gmail.com>
2024-09-03 09:05:09 +0530

    Handling Event in  React

---
+++ video112/Event-hanglilng/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  // const [name, setName] = useState("Raman")
+  const [form, setForm] = useState({})
+
+  const handleClick = ()=>{
+    alert("Hey i am clicked")
+  }
+  // const handleMouseOver = ()=>{
+  //   alert("Hey i am mouse over")
+  // }
+  const handleChange = (e)=>{
+    // setName(e.target.value)
+    setForm({...form, [e.target.name]: e.target.value})
+    console.log(form);
+    
+  }
+
+  return (
+    <>
+      <div className="button">
+        <button onClick={handleClick}>Click me</button>
+      </div>
+      {/* <div className="red" onMouseOver={handleMouseOver}>
+        I am a div
+      </div> */}
+      <input type="text" name='email' value={form.email?form.email: ""} onChange={handleChange} />
+      <input type="text" name='phone' value={form.phone?form.phone: ""} onChange={handleChange} />
+    </>
+  )
+}
+
+export default App
