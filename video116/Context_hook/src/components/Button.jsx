d763f9aa4ae538f24dc20b5b5bb6415336e527a8
Raman65 <rp0842262@gmail.com>
2024-09-04 09:16:44 +0530

    useContext hook in React

---
+++ video116/Context_hook/src/components/Button.jsx
@@ -0,0 +1,16 @@
+import React, {useContext}  from 'react';
+import Components1 from './Components1';
+import { counterContext } from '../../src/context/context';
+
+const Button = () => {
+  const value = useContext(counterContext)
+  return (
+    <>
+    <div>
+      <button onClick={() => value.setCount((count) => count + 1)}><span><Components1 /></span>I am a button</button>
+    </div>
+    </>
+  );
+}
+
+export default Button;
